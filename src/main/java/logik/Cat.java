7415e7a9e31a4a0bb022c1091f79aa75e4a73251
sxoxs <33289095+sxoxs@users.noreply.github.com>
2018-06-29 15:42:25 +0300

    готовое дз 1

---
+++ src/main/java/logik/Cat.java
@@ -0,0 +1,7 @@
+package logik;
+
+public class Cat extends Animals {
+    public Cat(String name, int maxRunDistance, int maxSwimDistance, int maxJumpHeight) {
+        super("Кот", name, maxRunDistance, maxSwimDistance, maxJumpHeight);
+    }
+}
